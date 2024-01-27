60ad4380273d6edb81e68b3493ce798cb3f39ead
atharvumap <atharvumap@gmail.com>
2024-01-27 10:43:41 -0500

    Read from data - Pickle

---
+++ reading_building_plan.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import pickle
+
+
+root = Tk()
+root.title("")
+width = root.winfo_screenwidth()  # width of screen
+height = root.winfo_screenheight()  # height of screen
+divisions = 100
+width_of_tile = width / divisions
+height_of_tile = height / divisions
+root.winfo_toplevel().geometry("%dx%d%+d%+d" % (width, height, 0, 0))
+
+my_canvas = Canvas(root, width = width, height = height)
+my_canvas.pack()
+
+with open('dummy.dat', 'rb') as p:
+    p1 = pickle.load(p)
+
+print("Canvas Dimensions:", my_canvas.winfo_width(), my_canvas.winfo_height())
+print(width, height)
+
+for tile in p1:
+    location = tile.get_location()
+    my_canvas.create_rectangle(location[0] * width_of_tile, location[1] * height_of_tile, location[0] * width_of_tile + width_of_tile,
+                               location[1] * height_of_tile + height_of_tile, fill=tile.color, outline = tile.color)
+
+
+root.mainloop()
+
+
+
