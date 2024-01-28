c25ce029b530c4cff6bf755509a0d4e55f773bad
atharvumap <atharvumap@gmail.com>
2024-01-28 08:48:42 -0500

    Final submission

--- Fire.py
+++ Fire.py
@@ -10,108 +10,118 @@ class Fire:
         self.canvas = canvas
         self.width = width
         self.height = height
+        # self.fire_values = fire_values
+
+    def get_fire_list(self):
+        return self.fire_list
 
     def spread_fire(self):
-        # print("fire spread called")
-        # print(self.fire_list)
-        fire_list2 = []
-        for tile in self.fire_list:
-            x = tile.x
-            y = tile.y
-            tile.set_on_fire(self.canvas, x * self.width, y * self.height, self.width, self.height, "red2")
-            # print(x, y)
-            # print(tile.flammable)
-            # top left cell
-            if x - 1 >= 0 and y - 1 >= 0 and self.all_tiles_dict[x - 1][y - 1] is not None and \
-                    self.all_tiles_dict[x - 1][
-                        y - 1].flammable == "Yes":
-                # print("past1")
-                self.all_tiles_dict[x - 1][y - 1].set_on_fire(self.canvas,
-                                                              self.all_tiles_dict[x - 1][y - 1].x * self.width,
-                                                              self.all_tiles_dict[x - 1][y - 1].y * self.height,
-                                                              self.width, self.height, "deep sky blue")
-                var = self.all_tiles_dict[x - 1][y - 1].x
-                var2 = self.all_tiles_dict[x - 1][y - 1].y
-                fire_list2.append(self.all_tiles_dict[x - 1][y - 1])
+            fire_list2 = []
+            for tile in self.fire_list:
+                x = tile.x
+                y = tile.y
 
-                # top up same column
-            if y - 1 >= 0 and self.all_tiles_dict[x][y - 1] is not None and self.all_tiles_dict[x][
-                y - 1].flammable == "Yes":
-                # print("past2")
-                self.all_tiles_dict[x][y - 1].set_on_fire(self.canvas, self.all_tiles_dict[x][y - 1].x * self.width,
-                                                          self.all_tiles_dict[x][y - 1].y * self.height, self.width,
-                                                          self.height, "gold")
-                fire_list2.append(self.all_tiles_dict[x][y - 1])
+                tile.set_on_fire(self.canvas, x * self.width, y * self.height, self.width, self.height, "red2")
+                # print(x, y)
+                # print(tile.flammable)
+                # top left cell
+                if x - 1 >= 0 and y - 1 >= 0 and self.all_tiles_dict[x - 1][y - 1] is not None and \
+                        self.all_tiles_dict[x - 1][
+                            y - 1].flammable == "Yes":
+                    # print("past1")
+                    self.all_tiles_dict[x - 1][y - 1].set_on_fire(self.canvas,
+                                                                  self.all_tiles_dict[x - 1][y - 1].x * self.width,
+                                                                  self.all_tiles_dict[x - 1][y - 1].y * self.height,
+                                                                  self.width, self.height, "tomato")
 
-                # top right
-            if x + 1 <= 99 and y - 1 >= 0 and self.all_tiles_dict[x + 1][y - 1] is not None and \
-                    self.all_tiles_dict[x + 1][
-                        y - 1].flammable == "Yes":
-                # print("past3")
-                self.all_tiles_dict[x + 1][y - 1].set_on_fire(self.canvas,
-                                                              self.all_tiles_dict[x + 1][y - 1].x * self.width,
-                                                              self.all_tiles_dict[x + 1][y - 1].y * self.height,
-                                                              self.width, self.height, "burlywood4")
-                var = self.all_tiles_dict[x + 1][y - 1].x
-                var2 = self.all_tiles_dict[x + 1][y - 1].y
-                fire_list2.append(self.all_tiles_dict[x + 1][y - 1])
-                # self.canvas.create_rectangle((x+1)*self.width, (y-1)*self.height, (x+1)*self.width + self.width, (y-1)*self.height + self.height, fill="Cyan2", outline="OrangeRed2")
+                    # top up same column
+                if y - 1 >= 0 and self.all_tiles_dict[x][y - 1] is not None and self.all_tiles_dict[x][
+                    y - 1].flammable == "Yes":
+                    # print("past2")
+                    self.all_tiles_dict[x][y - 1].set_on_fire(self.canvas,
+                                                              self.all_tiles_dict[x][y - 1].x * self.width,
+                                                              self.all_tiles_dict[x][y - 1].y * self.height,
+                                                              self.width,
+                                                              self.height, "gold")
+                    fire_list2.append(self.all_tiles_dict[x][y - 1])
 
-                # same level left
-            if x - 1 >= 0 and self.all_tiles_dict[x - 1][y] is not None and self.all_tiles_dict[x - 1][
-                y].flammable == "Yes":
-                # rint("past4")
-                self.all_tiles_dict[x - 1][y].set_on_fire(self.canvas, self.all_tiles_dict[x - 1][y].x * self.width,
-                                                          self.all_tiles_dict[x - 1][y].y * self.height, self.width,
-                                                          self.height, "indian red")
-                fire_list2.append(self.all_tiles_dict[x - 1][y])
+                    # top right
+                if x + 1 <= 99 and y - 1 >= 0 and self.all_tiles_dict[x + 1][y - 1] is not None and \
+                        self.all_tiles_dict[x + 1][
+                            y - 1].flammable == "Yes":
+                    # print("past3")
+                    self.all_tiles_dict[x + 1][y - 1].set_on_fire(self.canvas,
+                                                                  self.all_tiles_dict[x + 1][y - 1].x * self.width,
+                                                                  self.all_tiles_dict[x + 1][y - 1].y * self.height,
+                                                                  self.width, self.height, "burlywood4")
+                    var = self.all_tiles_dict[x + 1][y - 1].x
+                    var2 = self.all_tiles_dict[x + 1][y - 1].y
+                    fire_list2.append(self.all_tiles_dict[x + 1][y - 1])
+                    # self.canvas.create_rectangle((x+1)*self.width, (y-1)*self.height, (x+1)*self.width + self.width, (y-1)*self.height + self.height, fill="Cyan2", outline="OrangeRed2")
 
-            # same level right
-            if x + 1 <= 99 and self.all_tiles_dict[x + 1][y] is not None and self.all_tiles_dict[x + 1][
-                y].flammable == "Yes":
-                # print("past5")
-                self.all_tiles_dict[x + 1][y].set_on_fire(self.canvas, self.all_tiles_dict[x + 1][y].x * self.width,
-                                                          self.all_tiles_dict[x + 1][y].y * self.height, self.width,
-                                                          self.height, "orange")
-                fire_list2.append(self.all_tiles_dict[x + 1][y])
+                    # same level left
+                if x - 1 >= 0 and self.all_tiles_dict[x - 1][y] is not None and self.all_tiles_dict[x - 1][
+                    y].flammable == "Yes":
+                    # rint("past4")
+                    self.all_tiles_dict[x - 1][y].set_on_fire(self.canvas,
+                                                              self.all_tiles_dict[x - 1][y].x * self.width,
+                                                              self.all_tiles_dict[x - 1][y].y * self.height,
+                                                              self.width,
+                                                              self.height, "indian red")
+                    fire_list2.append(self.all_tiles_dict[x - 1][y])
 
-            # bottom left
-            if x - 1 >= 0 and y + 1 <= 99 and self.all_tiles_dict[x - 1][y + 1] is not None and \
-                    self.all_tiles_dict[x - 1][
-                        y + 1].flammable == "Yes":
-                # print("past6")
-                self.all_tiles_dict[x - 1][y + 1].set_on_fire(self.canvas,
-                                                              self.all_tiles_dict[x - 1][y + 1].x * self.width,
-                                                              self.all_tiles_dict[x - 1][y + 1].y * self.height,
-                                                              self.width, self.height, "hot pink")
-                fire_list2.append(self.all_tiles_dict[x - 1][y + 1])
+                # same level right
+                if x + 1 <= 99 and self.all_tiles_dict[x + 1][y] is not None and self.all_tiles_dict[x + 1][
+                    y].flammable == "Yes":
+                    # print("past5")
+                    self.all_tiles_dict[x + 1][y].set_on_fire(self.canvas,
+                                                              self.all_tiles_dict[x + 1][y].x * self.width,
+                                                              self.all_tiles_dict[x + 1][y].y * self.height,
+                                                              self.width,
+                                                              self.height, "orange")
+                    fire_list2.append(self.all_tiles_dict[x + 1][y])
 
-            # same column bottom down
-            if y + 1 <= 99 and self.all_tiles_dict[x][y + 1] is not None and self.all_tiles_dict[x][
-                y + 1].flammable == "Yes":
-                # print("past7")
-                self.all_tiles_dict[x][y + 1].set_on_fire(self.canvas, self.all_tiles_dict[x][y + 1].x * self.width,
-                                                          self.all_tiles_dict[x][y + 1].y * self.height, self.width,
-                                                          self.height, "dark violet")
-                fire_list2.append(self.all_tiles_dict[x][y + 1])
+                # bottom left
+                if x - 1 >= 0 and y + 1 <= 99 and self.all_tiles_dict[x - 1][y + 1] is not None and \
+                        self.all_tiles_dict[x - 1][
+                            y + 1].flammable == "Yes":
+                    # print("past6")
+                    self.all_tiles_dict[x - 1][y + 1].set_on_fire(self.canvas,
+                                                                  self.all_tiles_dict[x - 1][y + 1].x * self.width,
+                                                                  self.all_tiles_dict[x - 1][y + 1].y * self.height,
+                                                                  self.width, self.height, "hot pink")
+                    fire_list2.append(self.all_tiles_dict[x - 1][y + 1])
 
-            # bottom right
-            if x + 1 <= 99 and y + 1 <= 99 and self.all_tiles_dict[x + 1][y + 1] is not None and \
-                    self.all_tiles_dict[x + 1][
-                        y + 1].flammable == "Yes":
-                # print("past8")
-                self.all_tiles_dict[x + 1][y + 1].set_on_fire(self.canvas,
-                                                              self.all_tiles_dict[x + 1][y + 1].x * self.width,
-                                                              self.all_tiles_dict[x + 1][y + 1].y * self.height,
-                                                              self.width, self.height, "khaki")
-                fire_list2.append(self.all_tiles_dict[x + 1][y + 1])
-        # for tile in self.fire_list:
-        # print(tile.x, tile.y,)
-        # for tile in fire_list2:
-        # print(tile.x, tile.y, )
-        self.fire_list.clear()
-        # print(self.fire_list)
-        self.fire_list.extend(fire_list2)
-        # print(self.fire_list)
-        if len(self.fire_list) != 0:
-            self.spread_fire()
+                # same column bottom down
+                if y + 1 <= 99 and self.all_tiles_dict[x][y + 1] is not None and self.all_tiles_dict[x][
+                    y + 1].flammable == "Yes":
+                    # print("past7")
+                    self.all_tiles_dict[x][y + 1].set_on_fire(self.canvas,
+                                                              self.all_tiles_dict[x][y + 1].x * self.width,
+                                                              self.all_tiles_dict[x][y + 1].y * self.height,
+                                                              self.width,
+                                                              self.height, "dark violet")
+                    fire_list2.append(self.all_tiles_dict[x][y + 1])
+
+                # bottom right
+                if x + 1 <= 99 and y + 1 <= 99 and self.all_tiles_dict[x + 1][y + 1] is not None and \
+                        self.all_tiles_dict[x + 1][
+                            y + 1].flammable == "Yes":
+                    # print("past8")
+                    self.all_tiles_dict[x + 1][y + 1].set_on_fire(self.canvas,
+                                                                  self.all_tiles_dict[x + 1][y + 1].x * self.width,
+                                                                  self.all_tiles_dict[x + 1][y + 1].y * self.height,
+                                                                  self.width, self.height, "khaki")
+                    fire_list2.append(self.all_tiles_dict[x + 1][y + 1])
+
+            # for tile in self.fire_list:
+            # print(tile.x, tile.y,)
+            # for tile in fire_list2:
+            # print(tile.x, tile.y, )
+            self.fire_list.clear()
+            # print(self.fire_list)
+            self.fire_list.extend(fire_list2)
+            # print(self.fire_list)
+
+            if len(self.fire_list) != 0:
+                self.spread_fire()
--- Person.py
+++ Person.py
@@ -1,10 +1,13 @@
 import heapq
 import pickle
+import threading
 from vertices_and_edges import VerticesAndEdges
+import time
 
 
-class Person:
-    def __init__(self, x, y, all_tiles_list, all_tiles_dict, my_canvas, width_of_tile, height_of_tile):
+class Person(threading.Thread):
+    def __init__(self, x, y, all_tiles_list, all_tiles_dict, my_canvas, width_of_tile, height_of_tile): #fire_values
+        super().__init__()
         self.x_coord = x
         self.y_coord = y
         self.status = "Inside"
@@ -14,6 +17,7 @@ class Person:
         self.my_canvas = my_canvas
         self.width = width_of_tile
         self.height = height_of_tile
+        # self.fire_values = fire_values
 
         f = open("temp_for_file_name.txt", "r")
         name = f.read()
@@ -25,56 +29,95 @@ class Person:
             self.all_tiles_list = data["list"]
             self.all_tiles_dict = data["dictionary"]
 
+    def run(self):
+        self.makeNextMove()
+
     def makeNextMove(self):
-
-        data_set = VerticesAndEdges(self.all_tiles_list, self.all_tiles_dict)
-
-        lowest_cost_neighbour = None
+        #print(len(self.fire_values))
+        print("these many are in person")
+        time.sleep(0.1)
+        #self.fire_list.append(Fire.get_fire_list())
+        #data_set = VerticesAndEdges(self.all_tiles_list, self.all_tiles_dict)
+        #lowest_cost_neighbour = None
 
         curr = self.all_tiles_dict[self.x_coord][self.y_coord]
+        if curr.color == "maroon":
+            self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
+                                            self.x_coord * self.width + self.width,
+                                            self.y_coord * self.height + self.height, fill="maroon",
+                                            outline="maroon")
+            return
 
-        lowest_cost = 10000
-        left = False
-        print(data_set.tile_to_neighbouring_tiles_costs)
+        n = [self.all_tiles_dict[self.x_coord - 1][self.y_coord - 1],
+             self.all_tiles_dict[self.x_coord][self.y_coord - 1],
+             self.all_tiles_dict[self.x_coord + 1][self.y_coord - 1],
+             self.all_tiles_dict[self.x_coord - 1][self.y_coord], self.all_tiles_dict[self.x_coord + 1][self.y_coord],
+             self.all_tiles_dict[self.x_coord - 1][self.y_coord + 1],
+             self.all_tiles_dict[self.x_coord][self.y_coord + 1]]
 
+        exits = []
+        for tile in self.all_tiles_list:
+            if tile.color == "maroon":
+                exits.append(tile)
 
+        minCost = 100000
+        minCostNeigh = None
 
-        for neighbour in data_set.tile_to_neighbouring_tiles_costs[self.x_coord][self.y_coord]:
-            if lowest_cost_neighbour is None:
-                lowest_cost_neighbour = neighbour
-            elif neighbour.color == "maroon":
-                lowest_cost = \
-                    data_set.tile_to_neighbouring_tiles_costs[self.all_tiles_dict[self.x_coord][self.y_coord]][
-                        neighbour]
-                lowest_cost_neighbour = self.all_tiles_dict[self.x_coord][self.y_coord]
-                self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
-                                                self.x_coord * self.width + self.width,
-                                                self.y_coord * self.height + self.height, fill="maroon",
-                                                outline="maroon")
-                left = True
+        for neighbour in n:
+            time.sleep(0.1)
+            if neighbour is None:
+                continue
+
+            if minCostNeigh is None:
+                minCostNeigh = neighbour
+
+            # if neighbour in self.fire_values:
+            #     continue
+
+            neighbour.calculate_costs(exits)
+            cost = neighbour.cost_imposed_on_weight
+
+            # neighbour.calculate_costs(self.fire_values)
+
+            #print(f"before adding: {cost}")
+            # cost += 100 * neighbour.cost_imposed_on_weight
+            #print(f"after adding: {cost}")
+
+            # if neighbour in self.fire_values:
+            #     cost += self.fire_values[neighbour]
+
+            if neighbour.is_on_fire:
+                cost += 5000
+                n = [self.all_tiles_dict[neighbour.x_coord - 1][neighbour.y_coord - 1],
+                     self.all_tiles_dict[neighbour.x_coord][neighbour.y_coord - 1],
+                     self.all_tiles_dict[neighbour.x_coord + 1][neighbour.y_coord - 1],
+                     self.all_tiles_dict[neighbour.x_coord - 1][neighbour.y_coord],
+                     self.all_tiles_dict[neighbour.x_coord + 1][neighbour.y_coord],
+                     self.all_tiles_dict[neighbour.x_coord - 1][neighbour.y_coord + 1],
+                     self.all_tiles_dict[neighbour.x_coord][neighbour.y_coord + 1]]
+                for dn in n:
+                    if dn.is_on_fire:
+                        cost += 3000
+                    else:
+                        dn.cost_imposed_on_weight += 500
+            print(f"Cost here: {cost}")
+            if neighbour.color == "maroon":
+                minCostNeigh = neighbour
                 break
-            elif data_set.tile_to_neighbouring_tiles_costs[curr][neighbour] < lowest_cost:
-                lowest_cost = data_set.tile_to_neighbouring_tiles_costs[self.all_tiles_dict[self.x_coord][self.y_coord]][neighbour]
-                lowest_cost_neighbour = self.all_tiles_dict[self.x_coord][self.y_coord]
+            elif cost < minCost:
+                minCost = cost
+                minCostNeigh = neighbour
 
-        if not left:
-            self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
-                                            self.x_coord * self.width + self.width,
-                                            self.y_coord * self.height + self.height, fill="maroon",
-                                            outline="white")
-            self.x_coord = lowest_cost_neighbour.x_coord
-            self.y_coord = lowest_cost_neighbour.y_coord
-            self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
-                                            self.x_coord * self.width + self.width,
-                                            self.y_coord * self.height + self.height, fill="maroon",
-                                            outline="black")
-            self.makeNextMove()
+        #print(minCostNeigh.x, minCostNeigh.y)
 
-"""
-    def leave_building(self, exit_point):
-        self.location = exit_point
-        self.status = "Outside"
-        self.evacuation_status = True
-        print(f"{self.name} left the building through {exit_point}.")
-
-"""
+        self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
+                                        self.x_coord * self.width + self.width,
+                                        self.y_coord * self.height + self.height, fill="white",
+                                        outline="white")
+        self.x_coord = minCostNeigh.x
+        self.y_coord = minCostNeigh.y
+        self.my_canvas.create_rectangle(self.x_coord * self.width, self.y_coord * self.height,
+                                        self.x_coord * self.width + self.width,
+                                        self.y_coord * self.height + self.height, fill="black",
+                                        outline="black")
+        self.makeNextMove()
