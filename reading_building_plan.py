9ea87199a1b40cc9906a4b178472ce10a685fe4c
1909d <1909devpatel@gmail.com>
2024-01-27 21:07:58 -0500

    Revert "spawn humans and fire"
    This reverts commit 6c079c54

--- reading_building_plan.py
+++ reading_building_plan.py
@@ -10,69 +10,27 @@ root.title("")
 width = root.winfo_screenwidth()  # width of screen
 height = root.winfo_screenheight()  # height of screen
 divisions = 100
-width_of_tile = width // divisions
-height_of_tile = height // divisions
+width_of_tile = width / divisions
+height_of_tile = height / divisions
 root.winfo_toplevel().geometry("%dx%d%+d%+d" % (width, height, 0, 0))
 
-radio_selection = "human"
-
-var = StringVar()
-human_button = Radiobutton(root, text="Human", variable=var, value="human")
-human_button.pack(side=TOP, padx=10)
-fire_button = Radiobutton(root, text="Fire", variable=var, value="fire")
-fire_button.pack(side=TOP, padx=10)
-
-
-def submit():
-    root.unbind("<Button-1>")
-    human_button.destroy()
-    fire_button.destroy()
-    subButton.destroy()
-
-
-subButton = Button(root, text="Submit", command=submit)
-subButton.pack()
-
-my_canvas = Canvas(root, width=width, height=height)
+my_canvas = Canvas(root, width = width, height = height)
 my_canvas.pack()
 
 with open(name, "rb") as file:
     data = pickle.load(file)
     all_tiles_list = data["list"]
-    all_tiles_dict = data["dictionary"]
 
 print("Canvas Dimensions:", my_canvas.winfo_width(), my_canvas.winfo_height())
 print(width, height)
 
 for tile in all_tiles_list:
-    my_canvas.create_rectangle(tile.x * width_of_tile, tile.y * height_of_tile,
-                               tile.x * width_of_tile + width_of_tile,
-                               tile.y * height_of_tile + height_of_tile, fill=tile.color, outline=tile.color)
+    location = tile.get_location()
+    my_canvas.create_rectangle(location[0] * width_of_tile, location[1] * height_of_tile, location[0] * width_of_tile + width_of_tile,
+                               location[1] * height_of_tile + height_of_tile, fill=tile.color, outline = tile.color)
 
-
-def on_click(event):
-    global radio_selection
-    x, y = event.x, event.y
-    x_coord = int(x / width_of_tile)
-    y_coord = int(y / height_of_tile)
-
-    radio_selection = var.get()
-
-    print(all_tiles_dict)
-    print("look here")
-    print(x_coord, y_coord)
-
-    if radio_selection == "human":
-        all_tiles_dict[x_coord][y_coord].increment_population()
-        all_tiles_dict[x_coord][y_coord].put_human_on_tile(my_canvas, x, y)
-    else:
-        try:
-            all_tiles_dict[x_coord][y_coord].set_on_fire()
-            my_canvas.create_oval(x - 5, y - 5, x + 5, y + 5, fill="OrangeRed2", outline="OrangeRed2")
-        except:
-            pass
-
-
-root.bind("<Button-1>", on_click)
 
 root.mainloop()
+
+
+
--- vertices_and_edges.py
+++ vertices_and_edges.py
@@ -16,46 +16,40 @@ class VerticesAndEdges:
             # top left cell
             if x - 1 >= 0 and y - 1 >= 0 and self.all_tiles_dict[x - 1][y - 1] is not None and self.all_tiles_dict[x - 1][y - 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x - 1][y - 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y - 1]] = \
-                self.all_tiles_dict[x - 1][y - 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y - 1]] = 1
 
                 # top up same column
             if y - 1 >= 0 and self.all_tiles_dict[x][y - 1] is not None and self.all_tiles_dict[x][y - 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x][y - 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x][y - 1]] = self.all_tiles_dict[x][y - 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x][y - 1]] = 1
 
                 # top right
             if x + 1 <= 99 and y - 1 >= 0 and self.all_tiles_dict[x - 1][y - 1] is not None and self.all_tiles_dict[x - 1][y - 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x + 1][y - 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y - 1]] = \
-                self.all_tiles_dict[x + 1][y - 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y - 1]] = 1
 
                 # same level left
             if x - 1 >= 0 and self.all_tiles_dict[x - 1][y] is not None and self.all_tiles_dict[x - 1][y].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x - 1][y])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y]] = self.all_tiles_dict[x - 1][
-                    y].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y]] = 1
 
                 # same level right
             if x + 1 <= 99 and self.all_tiles_dict[x + 1][y] is not None and self.all_tiles_dict[x + 1][y].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x + 1][y])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y]] = self.all_tiles_dict[x + 1][
-                    y].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y]] = 1
 
                 # bottom left
             if x - 1 >= 0 and y + 1 <= 99 and self.all_tiles_dict[x - 1][y + 1] is not None and self.all_tiles_dict[x - 1][y + 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x - 1][y + 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y + 1]] = \
-                self.all_tiles_dict[x - 1][y + 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x - 1][y + 1]] = 1
 
             # same column bottom down
             if y + 1 <= 99 and self.all_tiles_dict[x][y + 1] is not None and self.all_tiles_dict[x][y + 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x][y + 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x][y + 1]] = self.all_tiles_dict[x][
-                    y + 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x][y + 1]] = 1
 
             # bottom right
             if x + 1 <= 99 and y + 1 <= 99 and self.all_tiles_dict[x + 1][y + 1] is not None and self.all_tiles_dict[x + 1][y + 1].walkable == True:
                 self.tile_to_neighbouring_tiles[tile].append(self.all_tiles_dict[x + 1][y + 1])
-                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y + 1]] = \
-                self.all_tiles_dict[x + 1][y + 1].cost_imposed_on_weight
+                self.tile_to_neighbouring_tiles_costs[tile][self.all_tiles_dict[x + 1][y + 1]] = 1
+
