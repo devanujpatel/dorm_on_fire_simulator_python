9c2fe9a00cbe46cb7b201c5018db19b4a14e5c22
1909d <1909devpatel@gmail.com>
2024-01-27 21:09:14 -0500

    Revert "spawn humans and fire"
    This reverts commit 6c079c54

--- Tiles.py
+++ Tiles.py
@@ -1,11 +1,3 @@
-import math
-
-
-def calculate_distance(x1, y1, x2, y2):
-    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return distance
-
-
 class Tile:
     def __init__(self, x, y, walkable, flammable, is_on_fire, has_human, color, id):
         self.x = x
@@ -14,41 +6,20 @@ class Tile:
         self.flammable = flammable
         self.is_on_fire = is_on_fire
         self.color = color
-        self.population = 0
+        self.has_human = has_human
         self.id = id
-        self.cost_imposed_on_weight = 0
 
-    def increment_population(self):
-        self.population += 1
-        self.cost_imposed_on_weight += 50
+    def is_walkable(self):
+        return self.walkable
 
-    def decrement_population(self):
-        self.population -= 1
-        self.cost_imposed_on_weight -= 10
+    def is_flammable(self):
+        return self.flammable
 
-    def set_on_fire(self):
-        self.is_on_fire = True
-        self.cost_imposed_on_weight += 500
+    def is_on_fire(self):
+        return self.is_on_fire
+    
+    def has_human(self):
+        return self.has_human
 
     def get_location(self):
         return self.x, self.y
-
-    def calculate_costs(self, exits):
-        min_dist = 123456
-        min_dist_exit = None
-        print(exits)
-        for each_exit in exits:
-            if min_dist == 123456:
-                min_dist_exit = each_exit
-            elif calculate_distance(self.x, self.y, each_exit.x, each_exit.y) < min_dist_exit:
-                min_dist = calculate_distance(self.x, self.y, each_exit.x, each_exit.y)
-                min_dist_exit = each_exit
-
-        self.cost_imposed_on_weight = calculate_distance(self.x, self.y, min_dist_exit.x, min_dist_exit.y)
-
-    def put_human_on_tile(self, canvas, x, y):
-        if not self.is_on_fire:
-            canvas.create_oval(x - 2, y - 2, x + 2, y + 2, fill="black", outline="black")
-
-    def remove_human_on_tile(self, canvas, x, y):
-        canvas.create_oval(x - 2, y - 2, x + 2, y + 2, fill="black", outline="black")
